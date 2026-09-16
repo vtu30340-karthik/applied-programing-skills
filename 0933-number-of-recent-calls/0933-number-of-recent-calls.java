85da8c2b848eb760ef358217471c5a0dc76f1344
vtu30340-karthik <vtu30340@veltech.edu.in>
2026-09-16 09:23:01 +0530

    Time: 19 ms (92.94%), Space: 59.8 MB (22.48%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class RecentCounter {
+    private Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
